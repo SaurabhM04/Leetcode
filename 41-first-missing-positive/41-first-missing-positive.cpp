31ff26272d92a20f6b43e3243a30c7db37a34a4d
Saurabh Mishra <88313164+SaurabhM04@users.noreply.github.com>
2022-08-20 00:10:41 +0530

    Time: 31 ms (99.97%), Space: 21.6 MB (99.93%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -1,14 +1,35 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        unordered_map<int,int>freq;
-        for(auto i:nums) {
-            freq[i]+=1;
+        
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            while(nums[i]>0 && nums[i]<=n && nums[nums[i]-1]!=nums[i])
+                swap(nums[nums[i]-1],nums[i]);
         }
-        int i=1;
-        while(freq.find(i)!=freq.end()) {
-            i++;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]!=i+1)
+                return i+1;
         }
-        return i;
+        return n+1;
+        
     }
 };
+
+//O(n) space
+// class Solution {
+// public:
+//     int firstMissingPositive(vector<int>& nums) {
+//         unordered_map<int,int>freq;
+//         for(auto i:nums) {
+//             freq[i]+=1;
+//         }
+//         int i=1;
+//         while(freq.find(i)!=freq.end()) {
+//             i++;
+//         }
+//         return i;
+//     }
+// };
