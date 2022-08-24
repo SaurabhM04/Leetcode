f5c3788bcd9db34679b8dc1d0cd721ba1da05057
Saurabh Mishra <88313164+SaurabhM04@users.noreply.github.com>
2022-08-24 18:34:54 +0530

    Time: 55 ms (10.63%), Space: 5.8 MB (88.51%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n<=0) {
+        if( n==1)
+            return true;
+        if(n<=0 or n%3) {
             return false;
         }
-       long long int max_power_three =pow(3,19);
-        if(max_power_three%n==0) {
-            return true;
-        }
-        return false;
+        
+       return isPowerOfThree(n/3);
            
     }
 };
