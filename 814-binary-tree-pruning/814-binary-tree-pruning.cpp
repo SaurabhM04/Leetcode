25861b580a2395e9f711aa0b921a4c04d18d47b0
Saurabh Mishra <88313164+SaurabhM04@users.noreply.github.com>
2022-09-10 02:38:36 +0530

    Time: 5 ms (37.87%), Space: 8.7 MB (63.44%) - LeetHub

--- 814-binary-tree-pruning/814-binary-tree-pruning.cpp
+++ 814-binary-tree-pruning/814-binary-tree-pruning.cpp
@@ -9,33 +9,17 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 class Solution {
 public:
-    int dfs(TreeNode* root,int sum)
-    {
-        if(!root)
-          return 0;
-        
-        if(root->left==NULL && root->right==NULL)
-            return root->val;
-        
-        int lsum= dfs(root->left,sum);
-        int rsum=dfs(root->right,sum);
-        
-        if(lsum==0 )
-            root->left=NULL;
-        if(rsum==0 )
-            root->right=NULL;
-        
-         sum=lsum+rsum+root->val;
-         return sum;
-        
-    }
     TreeNode* pruneTree(TreeNode* root) {
-        int sum=0;
-        int ans=dfs(root,sum);
-        if(ans==0)return NULL;
+       if(root!=NULL) {
+        
+       root->left=pruneTree(root->left);
+        
+        root->right=pruneTree(root->right);
+           if(root->left==NULL && root->right==NULL && root->val==0)
+            return NULL;
+       }
         return root;
     }
 };
