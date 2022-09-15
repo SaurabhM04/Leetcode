aa7e13037cb4b5a2515fdc85cd6d94a96422e202
Saurabh Mishra <88313164+SaurabhM04@users.noreply.github.com>
2022-09-16 00:52:30 +0530

    Time: 848 ms (27.95%), Space: 141.1 MB (77.89%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -12,18 +12,12 @@ public:
         }
         sort(changed.begin(),changed.end());
         for(int i=0;i<n;i++) {
-            if(mp[changed[i]]) {
-                if(mp.find(changed[i]*2)!=mp.end() and mp[changed[i]*2]) {
-                    mp[changed[i]*2]--;
-                    mp[changed[i]]--;
-                    ans.push_back(changed[i]);
-                }
-            }
-        }
-        for(auto i:mp) {
-            if(i.second!=0) {
-                return {};
-            }
+            if(mp[changed[i]]==0) continue;
+            if(mp[changed[i]*2]==0) return {};
+            mp[changed[i]]--;
+            mp[changed[i]*2]--;
+            ans.push_back(changed[i]);
+                
         }
         return ans;
     }
