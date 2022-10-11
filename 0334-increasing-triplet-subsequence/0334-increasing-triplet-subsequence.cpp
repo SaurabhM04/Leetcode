4f7c624e1946e70318bb8122ae95552722b15d4b
Saurabh Mishra <88313164+SaurabhM04@users.noreply.github.com>
2022-10-12 03:19:03 +0530

    Time: 172 ms (18.35%), Space: 61.4 MB (87.63%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool lis(vector<int>& nums) 
+    {
+        vector<int>ans;
+        ans.push_back(nums[0]);
+        for(int i=1;i<nums.size();i++)
+        {
+            if(ans.back()<nums[i])
+            {
+                ans.push_back(nums[i]);
+            }
+            else
+            {
+                int index=lower_bound(ans.begin(),ans.end(),nums[i])-ans.begin();
+                ans[index]=nums[i];
+            }
+            if(ans.size()>=3)
+                return true;
+        }
+        return false;
+    }
+    
+    bool lis1(vector<int>& nums)
+    {
+            int one = INT_MAX, two = INT_MAX;
+            for (auto num : nums) 
+            {
+                if (num < one)
+                    one = num;
+                else if (one < num && num < two)
+                    two = num;
+                else if (two < num)
+                    return true;
+            }
+            return false;
+    };
+ 
+    bool increasingTriplet(vector<int>& nums) {
+        return lis1(nums);
+        return lis(nums);
+    }
+};
