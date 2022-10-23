b5ec4c00bd174630f0e1412a47332cd0008bceba
Saurabh Mishra <88313164+SaurabhM04@users.noreply.github.com>
2022-10-23 21:25:18 +0530

    Time: 31 ms (95.07%), Space: 21.4 MB (86.33%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,24 +1,12 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-      
-        int n=nums.size();
-        unordered_map<int,int>m;
-        int ini_sum = accumulate(nums.begin(), nums.end(), 0);
-        for(auto x:nums)
-            m[x]++;
-        vector<int>v;
-        for(auto x:m)
-        {
-            if(x.second==2)
-                v.push_back(x.first);
+        long long int n=nums.size();
+        long long int sq=(n*(n+1)*(2*n+1))/6,sum=(n*(n+1))/2;
+        for(auto i:nums) {
+            sq-=i*i;
+            sum-=i;
         }
-        int sum=ini_sum-v[0];
-        
-        
-        int req_num = (n+1) * (n)/2 - sum;
-        v.push_back(req_num);
-        return v;
-        
+        return {(int)(sq/sum-sum)/2,(int)(sq/sum+sum)/2};
     }
 };
