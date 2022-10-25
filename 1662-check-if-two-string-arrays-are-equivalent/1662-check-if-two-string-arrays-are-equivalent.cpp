98a12be106f634f0f2a626bb276268a3f8a7b453
Saurabh Mishra <88313164+SaurabhM04@users.noreply.github.com>
2022-10-25 11:09:55 +0530

    Time: 11 ms (38.92%), Space: 11.4 MB (77.71%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
@@ -2,26 +2,29 @@ class Solution {
 public:
     bool arrayStringsAreEqual(vector<string>& word, vector<string>& word1) {
         string s="",s1="";
-        for(int i=0; i<word.size(); i++)
-        {
-            int n=word[i].length()-1;
-            int j=0;
-            while(j<=n){
-                s+=word[i][j];
-                j++;
-            }
-        }
-        for(int i=0; i<word1.size(); i++)
-        {
-            int n=word1[i].length()-1;
-            int j=0;
-            while(j<=n){
-                s1+=word1[i][j];
-                j++;
-            }
-        }
-        if(s==s1)
-            return true;
-        return false;
+        // for(int i=0; i<word.size(); i++)
+        // {
+        //     int n=word[i].length()-1;
+        //     int j=0;
+        //     while(j<=n){
+        //         s+=word[i][j];
+        //         j++;
+        //     }
+        // }
+        // for(int i=0; i<word1.size(); i++)
+        // {
+        //     int n=word1[i].length()-1;
+        //     int j=0;
+        //     while(j<=n){
+        //         s1+=word1[i][j];
+        //         j++;
+        //     }
+        // }
+        // return s==s1;
+        for(auto x:word)
+            s+=x;
+        for(auto x:word1)
+            s1+=x;
+        return s==s1;
     }
 };
