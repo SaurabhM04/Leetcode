c22a359c0cea3a56b1ce8ffd804537806d7db6f3
Saurabh Mishra <88313164+SaurabhM04@users.noreply.github.com>
2022-10-26 01:30:47 +0530

    Time: 452 ms (26.49%), Space: 64.6 MB (30.57%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -28,25 +28,57 @@
 //         return ans;
 //     }
 // };
+// // class Solution {
+// //     public:
+// //     int maxLength(vector<string>& A) {
+// //         vector<bitset<26>> dp = {bitset<26>()};
+// //         int res = 0;
+// //         for (auto& s : A) {
+// //             bitset<26> a;
+// //             for (char c : s)
+// //                 a.set(c - 'a');
+// //             int n = a.count();
+// //             if (n < s.size()) continue;
+
+// //             for (int i = dp.size() - 1; i >= 0; --i) {
+// //                 bitset c = dp[i];
+// //                 if ((c & a).any()) continue;
+// //                 dp.push_back(c | a);
+// //                 res = max(res, (int)c.count() + n);
+// //             }
+// //         }
+// //         return res;
+// //     }
+// // };
+
+
+
+
 class Solution {
     public:
-    int maxLength(vector<string>& A) {
-        vector<bitset<26>> dp = {bitset<26>()};
-        int res = 0;
-        for (auto& s : A) {
-            bitset<26> a;
-            for (char c : s)
-                a.set(c - 'a');
-            int n = a.count();
-            if (n < s.size()) continue;
-
-            for (int i = dp.size() - 1; i >= 0; --i) {
-                bitset c = dp[i];
-                if ((c & a).any()) continue;
-                dp.push_back(c | a);
-                res = max(res, (int)c.count() + n);
-            }
+    int ans=0;
+    bool unique(string s)
+    {
+        set<char>st;
+        for(auto i:s)
+        {
+            if(st.count(i)) return false;
+            st.insert(i);
         }
-        return res;
+        return true;
+    }
+    void rec(int idx,vector<string>&arr,string s)
+    {
+        if(!unique(s)) return ;
+        int sz=s.size();
+        ans=max(ans,sz);
+        for(int i=idx;i<arr.size();i++)
+        {
+            rec(i+1,arr,s+arr[i]);
+        }
+    }
+    int maxLength(vector<string>& A) {
+        rec(0,A,"");
+        return ans;
     }
 };
