11bc048e5a6a9290a9d95caf062c71e03c4b93e0
Saurabh Mishra <88313164+SaurabhM04@users.noreply.github.com>
2022-10-27 18:25:22 +0530

    Time: 3 ms (48.56%), Space: 6.6 MB (53.14%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -9,8 +9,7 @@ public:
             return;
         }            
         for(auto k : mapping[digits[index]-'0'])
-        {
-            
+        {         
             output+=k;
             solve(index+1,v,digits,output,mapping);
             output.pop_back();;
@@ -24,3 +23,53 @@ public:
         return v;
     }
 };
+// class Solution {
+// public:
+//     unordered_map<char,vector<char>> make_phone_pad() {
+//         unordered_map<char,vector<char>>phone;
+//         char ch='a';
+//         for(char i='2';i<='9';i++) {
+//             for(char j=ch;j<ch+3;j++) {
+//                 phone[i].push_back(j);
+//             }
+//             ch=ch+3;
+//             if(i=='7' or i=='9') {
+//                 phone[i].push_back(ch);
+//                 ch++;
+//             }    
+//         }
+        
+//         return phone;
+//     }
+    
+   
+//     void solve(int curr_index,string curr_string,string &digits,unordered_map<char,vector<char>>&pad,vector<string>&ans) {
+//         if(curr_index==digits.size()) {
+//             if(curr_string.size()) {
+//                 ans.push_back(curr_string);
+//             }
+//             return ;
+//         }
+//         for(auto i:pad[digits[curr_index]]) {
+//             curr_string+=i;
+//             // cout<<"HELLO";
+//             solve(curr_index+1,curr_string,digits,pad,ans);
+//             curr_string.pop_back();
+//         }
+//     }
+  
+    
+//     vector<string> letterCombinations(string digits) {
+//         unordered_map<char,vector<char>>pad=make_phone_pad();
+//         // for(auto i:pad) {
+//         //     cout<<i.first<<" -> ";
+//         //     for(auto j:i.second) {
+//         //         cout<<j<<" ";
+//         //     }
+//         //     cout<<endl;
+//         // }
+//         vector<string>ans;
+//         solve(0,"",digits,pad,ans);
+//         return ans;
+//     }
+// };
