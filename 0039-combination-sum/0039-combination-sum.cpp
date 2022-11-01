60c6cbea25d2714101488767f4fab11b37d5428b
Saurabh Mishra <88313164+SaurabhM04@users.noreply.github.com>
2022-11-02 01:07:56 +0530

    Time: 23 ms (36.69%), Space: 16 MB (35.48%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>>ans;
+    void rec(int idx,vector<int>& candidates,vector<int>v, int target)
+    {
+        if(target<0) return;
+        if(target==0) 
+        {
+            ans.push_back(v);
+            return;
+        }
+        for(int i=idx;i<candidates.size();i++)
+        {
+            v.push_back(candidates[i]);
+            target-=candidates[i];
+            rec(i,candidates,v,target);
+            v.pop_back();
+            target+=candidates[i];
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        int idx=0;
+        rec(0,candidates,{},target);
+        return ans;
+    }
+};
