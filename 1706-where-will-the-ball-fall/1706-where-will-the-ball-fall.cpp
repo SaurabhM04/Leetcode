e8bb8175a236d814efe5e1a0bc4fe84e740efb51
Saurabh Mishra <88313164+SaurabhM04@users.noreply.github.com>
2022-11-02 05:18:40 +0530

    Time: 29 ms (87.57%), Space: 13.2 MB (81.02%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -1,26 +1,52 @@
+// class Solution {    
+// public:
+//     int recur(vector<vector<int>>& grid,int i,int j,vector<vector<int>>&dp,int m,int n)
+//     {
+//             if(i==m){
+//              return j;
+//             }
+//             if(dp[i][j]!=-1)
+//              return dp[i][j];
+//             if(j+1<n && grid[i][j]==1 && grid[i][j+1]==1)
+//              return dp[i][j]=recur(grid,i+1,j+1,dp,m,n);
+//             if(j-1>=0 && grid[i][j]==-1 && grid[i][j-1]==-1)
+//              return dp[i][j]=recur(grid,i+1,j-1,dp,m,n);
+//             else 
+//              return -1;      
+//     }
+//     vector<int> findBall(vector<vector<int>>& grid) {
+//         vector<int>ans;
+//         int m=grid.size(),n=grid[0].size();
+//         vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+//         for(int i=0; i<grid[0].size(); i++)
+//         {
+//            ans.push_back(recur(grid,0,i,dp,m,n));
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
     
 public:
-    int recur(vector<vector<int>>& grid,int i,int j,vector<vector<int>>&dp,int m,int n)
+    int recur(vector<vector<int>>& grid,int i,int j)
     {
-         if(i==m)
+         if(i==grid.size()){
             return j;
-           if(dp[i][j]!=-1)
-             return dp[i][j];
-            if(j+1<n && grid[i][j]==1 && grid[i][j+1]==1)
-            return dp[i][j]=recur(grid,i+1,j+1,dp,m,n);
+             cout<<j<<endl;
+         }
+            if(j+1<grid[0].size() && grid[i][j]==1 && grid[i][j+1]==1)
+            return recur(grid,i+1,j+1);
             if(j-1>=0 && grid[i][j]==-1 && grid[i][j-1]==-1)
-            return dp[i][j]=recur(grid,i+1,j-1,dp,m,n);
+            return recur(grid,i+1,j-1);
             else return -1;
       
     }
     vector<int> findBall(vector<vector<int>>& grid) {
         vector<int>ans;
-        int m=grid.size(),n=grid[0].size();
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
         for(int i=0; i<grid[0].size(); i++)
         {
-           ans.push_back(recur(grid,0,i,dp,m,n));
+           ans.push_back(recur(grid,0,i));
         }
         return ans;
     }
