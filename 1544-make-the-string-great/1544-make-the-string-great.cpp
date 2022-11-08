7a02ed18fb420846bdd506d7096d8387e76b7311
Saurabh Mishra <88313164+SaurabhM04@users.noreply.github.com>
2022-11-08 16:48:28 +0530

    Time: 4 ms (64.51%), Space: 6.4 MB (54.48%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,15 +1,37 @@
 class Solution {
 public:
     string makeGood(string s) {
-        int i=0;
-        while(i<s.size()) {
-            if(i<s.size()-1 and (s[i]==char(s[i+1]+32) or s[i]==char(s[i+1]-32))) {
-                s.erase(i,2);
-                i=max(0,i-1);
-            }else {
-                i+=1;
+        stack<char>st;
+        st.push(s[0]);
+        for(int i=1; i<s.size(); i++){           
+            if(!st.empty()&&(st.top()==s[i]-32||st.top()==s[i]+32)){
+                st.pop();
             }  
+            else{
+                st.push(s[i]);
+            }
         }
-        return s;
+        string str="";
+        while(!st.empty()){
+            str+=st.top();
+            st.pop();
+        }
+        reverse(str.begin(),str.end());
+        return str;
     }
 };
+// class Solution {
+// public:
+//     string makeGood(string s) {
+//         int i=0;
+//         while(i<s.size()) {
+//             if(i<s.size()-1 and (s[i]==char(s[i+1]+32) or s[i]==char(s[i+1]-32))) {
+//                 s.erase(i,2);
+//                 i=max(0,i-1);
+//             }else {
+//                 i+=1;
+//             }  
+//         }
+//         return s;
+//     }
+// };
