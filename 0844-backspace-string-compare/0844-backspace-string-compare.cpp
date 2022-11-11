80a7116dacfcfdc73cc16116ec1985ae4da90a69
Saurabh Mishra <88313164+SaurabhM04@users.noreply.github.com>
2022-11-11 18:13:49 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (6.26%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -1,9 +1,7 @@
-
 class Solution {
 public:
-    bool backspaceCompare(string s, string t) {
-        
-        stack<char>st,st1;
+    string dead(string s){
+        stack<char>st;
         for(int i=0; i<s.size(); i++){
             if(!st.empty() && s[i]=='#'){
                 st.pop();
@@ -12,15 +10,42 @@ public:
                 st.push(s[i]);
             }
         }
-        for(int i=0; i<t.size(); i++){
-            if(!st1.empty() && t[i]=='#'){
-                st1.pop();
-            }
-            else if(t[i]!='#'){
-                st1.push(t[i]);
-            }
+        string str="";
+        while(!st.empty()){
+            str+=st.top();
+            st.pop();
         }
-        return st==st1;
+        //cout<<str<<" "<<endl;
+        return str;
+    }
+    bool backspaceCompare(string s, string t) {
+       
+        return dead(s)==dead(t);
         
     }
 };
+// class Solution {
+// public:
+//     bool backspaceCompare(string s, string t) {
+        
+//         stack<char>st,st1;
+//         for(int i=0; i<s.size(); i++){
+//             if(!st.empty() && s[i]=='#'){
+//                 st.pop();
+//             }
+//             else{
+//                 st.push(s[i]);
+//             }
+//         }
+//         for(int i=0; i<t.size(); i++){
+//             if(!st1.empty() && t[i]=='#'){
+//                 st1.pop();
+//             }
+//             else{
+//                 st1.push(t[i]);
+//             }
+//         }
+//         return st==st1;
+        
+//     }
+// };
