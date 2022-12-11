d4a50364cffdd47bf0d023477c9189f38fc96377
Saurabh Mishra <88313164+SaurabhM04@users.noreply.github.com>
2022-12-12 04:47:37 +0530

    Time: 27 ms (98.86%), Space: 30.8 MB (63.35%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -11,25 +11,60 @@
  */
 class Solution {
 public:
-    int countNodes(TreeNode* root) {
-        queue<TreeNode*>q;
-        int ans=0;
-        if(!root)
-            return 0;
-        q.push(root);
-        //ans++;
-        while(!q.empty()){
-            int n=q.size();
-            while(n--){
-                TreeNode* temp=q.front();
-                q.pop();
-                ans++;
-                if(temp->left)
-                    q.push(temp->left);
-                if(temp->right)
-                    q.push(temp->right);
-            }
+    int leftHeight(TreeNode*root) {
+        int lh =0;
+        while(root)
+        {
+            lh++;
+            root= root->left;
         }
-        return ans;
+        return lh;
+    }
+    int rightHeight(TreeNode*root) {
+        int rh =0;
+        while(root)
+        {
+            rh++;
+            root = root->right;
+        }
+        return rh;
+    }
+    int countNodes(TreeNode* root) {
+        if(root == NULL)
+        {
+            return 0;
+        }
+      int lh = leftHeight(root);
+      int rh = rightHeight(root);
+      if(lh == rh)
+      {
+       return (1<<lh) -1;   
+      }
+        return 1 + countNodes(root->left) + countNodes(root->right);
     }
 };
+//using bfs
+// class Solution {
+// public:
+//     int countNodes(TreeNode* root) {
+//         queue<TreeNode*>q;
+//         int ans=0;
+//         if(!root)
+//             return 0;
+//         q.push(root);
+//         //ans++;
+//         while(!q.empty()){
+//             int n=q.size();
+//             while(n--){
+//                 TreeNode* temp=q.front();
+//                 q.pop();
+//                 ans++;
+//                 if(temp->left)
+//                     q.push(temp->left);
+//                 if(temp->right)
+//                     q.push(temp->right);
+//             }
+//         }
+//         return ans;
+//     }
+// };
