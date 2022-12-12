cb784dee2184d16a3944ffe326618404dc1bf3e6
Saurabh Mishra <88313164+SaurabhM04@users.noreply.github.com>
2022-12-12 06:32:48 +0530

    Time: 0 ms (100.00%), Space: 6 MB (38.51%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,29 @@
+// class Solution {
+// public:
+//     int climbStairs(int n) {
+//         vector<int>v;
+//         v.push_back(1);
+//         v.push_back(2);
+//         v.push_back(3);
+//         for(int i=3; i<n; i++){
+//             v.push_back(v[i-1]+v[i-2]);
+//         }
+//         return v[n-1];
+//     }
+// };
+
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int>v;
-        v.push_back(1);
-        v.push_back(2);
-        v.push_back(3);
-        for(int i=3; i<n; i++){
-            v.push_back(v[i-1]+v[i-2]);
-        }
-        return v[n-1];
+      
+     int m,f=1,s=2;
+     if(n==1 ||n==2)
+        return n;
+     for(int i=0;i<n-2;i++){
+         m=f+s;
+         f=s;
+         s=m;
+     }
+     return m;   
     }
-};
+};  
