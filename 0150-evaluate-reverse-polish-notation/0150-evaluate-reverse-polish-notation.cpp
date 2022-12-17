e151d57cec394c5bb442628943b1032ffd222075
Saurabh Mishra <88313164+SaurabhM04@users.noreply.github.com>
2022-12-17 11:31:31 +0530

    Time: 7 ms (98.04%), Space: 12.2 MB (17.58%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,28 +1,59 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        int n=tokens.size();
-        stack<string>st;
-        for(int i=0;i<n;i++)
-        {
-            if(tokens[i]=="+"||tokens[i]=="-"||tokens[i]=="*"||tokens[i]=="/")
-            {
-                long long a=stoll(st.top());
-                st.pop();
-                long long b=stoll(st.top());
-                st.pop();
-                if(tokens[i]=="+") a=a+b;
-                if(tokens[i]=="-") a=b-a;
-                if(tokens[i]=="*") a=a*b;
-                if(tokens[i]=="/") a=b/a;
-                st.push(to_string(a));
+       stack<long long int> s;
+        for(int i=0;i<tokens.size();i++){
+            if(tokens[i]=="+" || tokens[i]=="-" || tokens[i]=="*" || tokens[i]=="/"){
+                long long int a = s.top();
+                s.pop();
+                long long int b = s.top();
+                s.pop();
+                //int res;
+                if(tokens[i] == "+"){
+                    s.push(a+b);
+                }
+                else if(tokens[i] == "-"){
+                    s.push(b-a);
+                }
+                else if(tokens[i] == "*"){
+                    s.push(a*b);
+                }
+                else if(tokens[i] == "/"){
+                    s.push(b/a);
+                }
             }
-            else 
-            {
-                st.push(tokens[i]);
-            }
+            else
+                s.push((long long)stoi(tokens[i]));
         }
-        
-        return stoll(st.top());
+        return s.top(); 
     }
 };
+
+// class Solution {
+// public:
+//     int evalRPN(vector<string>& tokens) {
+//         int n=tokens.size();
+//         stack<string>st;
+//         for(int i=0;i<n;i++)
+//         {
+//             if(tokens[i]=="+"||tokens[i]=="-"||tokens[i]=="*"||tokens[i]=="/")
+//             {
+//                 long long a=stoll(st.top());
+//                 st.pop();
+//                 long long b=stoll(st.top());
+//                 st.pop();
+//                 if(tokens[i]=="+") a=a+b;
+//                 if(tokens[i]=="-") a=b-a;
+//                 if(tokens[i]=="*") a=a*b;
+//                 if(tokens[i]=="/") a=b/a;
+//                 st.push(to_string(a));
+//             }
+//             else 
+//             {
+//                 st.push(tokens[i]);
+//             }
+//         }
+        
+//         return stoll(st.top());
+//     }
+// };
