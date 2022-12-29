74d373dbee65de748399110f02cb2370dc152ebe
Saurabh Mishra <88313164+SaurabhM04@users.noreply.github.com>
2022-12-30 02:23:09 +0530

    Time: 142 ms (61.63%), Space: 44 MB (32.62%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -6,31 +6,19 @@ public:
         int count = 0;
         int n = nums.size();
 
-    for(int i=0;i<n;i++){
-        sum += nums[i];
-        int req = sum - k;
-        if(sum == k){
-            count += 1;
-        }      
-        // auto itr = m.find(req);
-        if(m.find(req) != m.end()){
-            count += m[sum-k];
+        for(int i=0;i<n;i++){
+            sum += nums[i];
+            int req = sum - k;
+            if(sum == k){
+                count += 1;
+            }      
+            // auto itr = m.find(req);
+            if(m.find(req) != m.end()){
+                count += m[sum-k];
+            }
+            m[sum]++;
         }
-        m[sum]++;
-    }
 
-     return count;
-        
-        // int count  = 0;
-        //    for(int i = 0;i<nums.size();i++){
-        //           int sum = 0;
-        //         for(int j = i;j<nums.size();j++){
-        //               sum += nums[j]; 
-        //             if(sum == k){
-        //                 count++;
-        //         }
-        //      }   
-        //    }
-        // return count;
+        return count;
     }
 };
