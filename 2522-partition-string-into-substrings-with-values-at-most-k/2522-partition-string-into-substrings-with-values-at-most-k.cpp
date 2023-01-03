0f243a2f895fec557514fdde9b9a7948d2a44d86
Saurabh Mishra <88313164+SaurabhM04@users.noreply.github.com>
2023-01-03 10:12:30 +0530

    Time: 43 ms (29.01%), Space: 8.9 MB (80.04%) - LeetHub

--- 2522-partition-string-into-substrings-with-values-at-most-k/2522-partition-string-into-substrings-with-values-at-most-k.cpp
+++ 2522-partition-string-into-substrings-with-values-at-most-k/2522-partition-string-into-substrings-with-values-at-most-k.cpp
@@ -1,24 +1,41 @@
+// class Solution {
+// public:
+//     int minimumPartition(string s, int k) {
+//         int n=s.length();
+//         string str="";
+//         long long int temp=0;
+//         int res=0;
+//         for(int i=0; i<n; i++){
+//             if(s[i]-'0'>k)
+//                 return -1;
+//             str+=s[i];
+//             stringstream x(str);
+//             x>>temp;
+//             if(temp>k){
+//                 res++;
+//                 str=s[i];
+//             }
+//         }
+        
+//         res++;
+        
+//         return res;
+//     }
+// };
+
 class Solution {
 public:
     int minimumPartition(string s, int k) {
-        int n=s.length();
-        string str="";
-        long long int temp=0;
-        int res=0;
-        for(int i=0; i<n; i++){
-            if(s[i]-'0'>k)
-                return -1;
-            str+=s[i];
-            stringstream x(str);
-            x>>temp;
-            if(temp>k){
+        long long int n=s.size(),curr_num=0,res=0;
+        for(int i=0;i<n;i++) {
+            curr_num=curr_num*10+(s[i]-'0');
+            if(curr_num>k) {
                 res++;
-                str=s[i];
+                curr_num=s[i]-'0';
+                if(curr_num>k) 
+                    return -1;
             }
         }
-        
-        res++;
-        
-        return res;
+        return res+1;
     }
 };
