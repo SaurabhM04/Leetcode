6e6ed5ab017b5226c871314137e24977eac47a7f
Saurabh Mishra <88313164+SaurabhM04@users.noreply.github.com>
2023-01-24 08:22:46 +0530

    Time: 536 ms (5.02%), Space: 69 MB (21.26%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,16 +1,34 @@
+// class Solution {
+// public:
+//     int findJudge(int n, vector<vector<int>>& trust) {
+//         vector<int>vis(n+1,0);
+//         for(auto &i:trust)
+//         {
+//             vis[i[0]]--;
+//             vis[i[1]]++;
+//         }
+//         for(int i=1;i<=n;i++)
+//         {
+//             if(vis[i]==n-1) return i;
+//         }
+//         return -1;
+//     }
+// };
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<int>vis(n+1,0);
-        for(auto &i:trust)
-        {
-            vis[i[0]]--;
-            vis[i[1]]++;
+        vector<int>trustme(n+1,0),trustother(n+1,0);
+        for(auto i:trust) {
+            trustme[i[1]]++;
+            trustother[i[0]]++;
         }
-        for(int i=1;i<=n;i++)
-        {
-            if(vis[i]==n-1) return i;
+        int judge=-1;
+        for(int i=1;i<=n;i++) {
+            if(trustme[i]==n-1 and trustother[i]==0) {
+                return i;
+            }
         }
-        return -1;
+        return -1;;
+
     }
 };
