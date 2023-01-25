c6a03754c7e6443bb9dd6dff2730b0f320e4be8f
Saurabh Mishra <88313164+SaurabhM04@users.noreply.github.com>
2023-01-25 15:21:52 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (90.93%) - LeetHub

---
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minOperations(int n) {
+        vector<int>v;
+        int mid;
+        if(n%2){
+            int x=(n/2);
+            mid=2*x+1;
+        }
+        else{
+            int y=n/2;
+            mid=(2*y);
+        }
+        int ans=0;
+        for(int i=0; i<n/2; i++){
+            ans+=(mid-(2*i+1));
+        }
+        return ans;
+    }
+};
