3fc2c3aacb0542a32340bd646cb3e46777d2c527
Saurabh Mishra <88313164+SaurabhM04@users.noreply.github.com>
2023-02-08 17:10:03 +0530

    Time: 19 ms (69.69%), Space: 16.6 MB (56.17%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,3 +1,15 @@
+/*
+here maxi tells us that in case we want to jump then what is the maximum index we can go to.
+limit tells us, till which index we are allowed to go currently.
+
+initially we are on index 0 so our limit is also 0 as we know that we have to jump from index 0 to somewhere and that somewhere will be told by maxi.
+after that again when we will touch our limit we will again update our jump count and update our limit to maxi.
+
+edge case when our limit is n-1 , it means that we could reach n-1 , so we have to apply one extra condition of limit != n-1 as we don't want to update our jump count there.
+*/
+
+
+
 class Solution {
 public:
     int jump(vector<int>& nums) {
